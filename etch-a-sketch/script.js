94885e0890d7dcae4a85f02749e40ba3d5513067
Samir Sharan <11716061+insignias@users.noreply.github.com>
2023-11-12 02:56:49 -0500

    Completed Project

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,53 @@
+const sketchpad = document.querySelector(".sketchpad");
+const colorpicker = document.querySelector("#colorpicker");
+const eraser = document.querySelector(".erase");
+const clear = document.querySelector(".clear")
+
+function buildGrid(val) {
+    sketchpad.style.gridTemplateColumns = `repeat(${val}, 2fr)`;
+    sketchpad.style.gridTemplateRows = `repeat(${val}, 2fr)`;
+    for(let i=0; i< val*val; i++) {
+        let box = document.createElement("div");
+        box.className = "box"  
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = colorpicker.value
+        })
+        
+        colorpicker.addEventListener("input", () => {
+            box.addEventListener("mouseover", () => {
+                box.style.backgroundColor = colorpicker.value 
+            })
+        })
+
+        // Eraser
+        eraser.addEventListener("click", () => {
+            box.addEventListener("mouseover", () => {
+                box.style.backgroundColor = "white"
+            })
+        })
+
+        // Clear
+        clear.addEventListener("click", () => {
+            box.style.backgroundColor = "white"
+        })
+        sketchpad.appendChild(box)
+    }
+}
+
+function cleanupGrid() {
+    while(sketchpad.firstChild) sketchpad.removeChild(sketchpad.firstChild)
+    colorpicker.value = "black"
+}
+
+const range = document.querySelector("#range")
+const screenValue = document.querySelector(".value");
+range.addEventListener("input", () => {
+    let val = document.getElementById('range').value;
+    console.log(val)
+    screenValue.textContent = val
+    cleanupGrid()
+    buildGrid(val)
+})
+
+
+buildGrid(20)
