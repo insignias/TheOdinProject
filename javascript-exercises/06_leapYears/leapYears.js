e5d9411b62449fbd0fb60d6bd5d1510d0d979a39
Samir Sharan <11716061+insignias@users.noreply.github.com>
2023-11-12 03:29:58 -0500

    remove javascript-exercises

--- javascript-exercises/06_leapYears/leapYears.js
+++
@@ -1,7 +0,0 @@
-const leapYears = function(year) {
-    if (typeof year !== 'number') return "ERROR"
-    return year%4 === 0 && (year%100 !== 0 || year%400 === 0 )
-};
-
-// Do not edit below this line
-module.exports = leapYears;
